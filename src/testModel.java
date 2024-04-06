3f60636200edbf93067d5c1dbfde9668468402a2
xurpas7 <57973174+xurpas7@users.noreply.github.com>
2024-04-06 17:08:16 +0800

    2024.04.06

--- src/testModel.java
+++ src/testModel.java
@@ -2,21 +2,18 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import org.guanzon.clients.Model_Client_Mobile;
 import org.guanzon.clients.Model_Client_Master;
-import org.guanzon.validators.ValidatorFactory;
 import org.guanzon.validators.ValidatorInterface;
-import org.rmj.appdriver.MiscUtil;
-import org.rmj.appdriver.SQLUtil;
 
 public class testModel {
-    public static void main (String [] args){
-        // Connection parameters
-        String url = "jdbc:mysql://172.16.0.14:3306/CASys_DBF";
-        String username = "username";
-        String password = "password";
-
+    public static void main (String [] args){        
         try {
+            loadConfig();
+            
+            String url = System.getProperty("sys.database.name");
+            String username = System.getProperty("sys.database.user");
+            String password = System.getProperty("sys.database.pass");
+
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to server.");
             
@@ -26,60 +23,70 @@ public class testModel {
             //initialize entity
             Model_Client_Master master = new Model_Client_Master(connection);
             System.out.println("Connected to master model class.");
-            
-            master.setClientID("M00124000001");
-            master.setClientType("0");
-            master.setLastName("Cuison");
-            master.setFirstName("Michael");
-            master.setMiddleName("Medina");
-            master.setSuffixName("Jr.");
-            master.setFullName("Cuison, Michael Jr. Medina");
-            master.setSex("0");
-            master.setCivilStatus("0");
-            master.setCitizenshipID("01");
-            master.setCitizenshipName("Philippines");
-            master.setBirthDate(SQLUtil.toDate("2025-01-01", SQLUtil.FORMAT_SHORT_DATE));
-            master.setBirthPlaceID("0314");
-            master.setBirthPlaceName("Dagupan City, Pangasinan");
-            master.setAdditionalInfo("Anak ni mac gwapo.");
-            master.setSpouseID("");
-            master.setSpouseName("");
-            master.setTaxIDNumber("");
-            master.setSparepartsSalesClient(true);
-            master.setModifiedBy("M001111122");
-            master.setModifiedDate(SQLUtil.toDate("2024-03-29 19:15:00", SQLUtil.FORMAT_TIMESTAMP));
-            
-            validator = ValidatorFactory.make(ValidatorFactory.TYPE.Client_Master, master);
-            if (!validator.isEntryOkay()){
-                System.err.println(validator.getMessage());
-                System.exit(1);
-            }
-            
-            System.out.println(MiscUtil.makeSQL(master, "xBirthPlc;xCitizenx;xSpouseNm"));
-            
-            Model_Client_Mobile mobile = new Model_Client_Mobile(connection);
-            System.out.println("Connected to mobile model class.");
-            
-            mobile.setMobileID("M00124000001");
-            mobile.setClientID("M00124000001");
-            mobile.setContactNo("09260375777");
-            mobile.setContactType("0");
-            mobile.setOwnership("0");
-            mobile.setPrimary(true);
-            mobile.setPriority(1);
-            mobile.setMarketingRecipient(true);
-            mobile.setModifiedDate(SQLUtil.toDate("2024-03-29 19:15:00", SQLUtil.FORMAT_TIMESTAMP));
-            
-            validator = ValidatorFactory.make(ValidatorFactory.TYPE.Client_Mobile, mobile);
-            if (!validator.isEntryOkay()){
-                System.err.println(validator.getMessage());
-                System.exit(1);
-            }
-            
-            
-            System.out.println(MiscUtil.makeSQL(mobile));
+            master.list();
+//            
+//            master.setClientID("M00124000001");
+//            master.setClientType("0");
+//            master.setLastName("Cuison");
+//            master.setFirstName("Michael");
+//            master.setMiddleName("Medina");
+//            master.setSuffixName("Jr.");
+//            master.setFullName("Cuison, Michael Jr. Medina");
+//            master.setSex("0");
+//            master.setCivilStatus("0");
+//            master.setCitizenshipID("01");
+//            master.setCitizenshipName("Philippines");
+//            master.setBirthDate(SQLUtil.toDate("2025-01-01", SQLUtil.FORMAT_SHORT_DATE));
+//            master.setBirthPlaceID("0314");
+//            master.setBirthPlaceName("Dagupan City, Pangasinan");
+//            master.setAdditionalInfo("Anak ni mac gwapo.");
+//            master.setSpouseID("");
+//            master.setSpouseName("");
+//            master.setTaxIDNumber("");
+//            master.setSparepartsSalesClient(true);
+//            master.setModifiedBy("M001111122");
+//            master.setModifiedDate(SQLUtil.toDate("2024-03-29 19:15:00", SQLUtil.FORMAT_TIMESTAMP));
+//            
+//            validator = ValidatorFactory.make(ValidatorFactory.TYPE.Client_Master, master);
+//            if (!validator.isEntryOkay()){
+//                System.err.println(validator.getMessage());
+//                System.exit(1);
+//            }
+//            
+//            System.out.println(MiscUtil.makeSQL(master, "xBirthPlc;xCitizenx;xSpouseNm"));
+//            
+//            Model_Client_Mobile mobile = new Model_Client_Mobile(connection);
+//            System.out.println("Connected to mobile model class.");
+//            
+//            mobile.setMobileID("M00124000001");
+//            mobile.setClientID("M00124000001");
+//            mobile.setContactNo("09260375777");
+//            mobile.setContactType("0");
+//            mobile.setOwnership("0");
+//            mobile.setPrimary(true);
+//            mobile.setPriority(1);
+//            mobile.setMarketingRecipient(true);
+//            mobile.setModifiedDate(SQLUtil.toDate("2024-03-29 19:15:00", SQLUtil.FORMAT_TIMESTAMP));
+//            
+//            validator = ValidatorFactory.make(ValidatorFactory.TYPE.Client_Mobile, mobile);
+//            if (!validator.isEntryOkay()){
+//                System.err.println(validator.getMessage());
+//                System.exit(1);
+//            }
+//            
+//            
+//            System.out.println(MiscUtil.makeSQL(mobile));
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+    
+    private static void loadConfig(){
+        System.setProperty("sys.database.name", "jdbc:mysql://172.16.0.14:3306/CASys_DBF");
+        System.setProperty("sys.database.user", "sa");
+        System.setProperty("sys.database.pass", "Atsp,imrtptd");
+
+        System.setProperty("sys.default.path.config", "D:/GGC_Java_Systems/");
+        System.setProperty("sys.default.path.metadata", System.getProperty("sys.default.path.config") + "config/metadata/");
     }
 }
---
+++ src/testResultSet2XML.java
@@ -0,0 +1,72 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.rmj.appdriver.MiscUtil;
+
+public class testResultSet2XML {
+    public static void main (String [] args){
+        final String DIRECTORY = "D:/GGC_Java_Systems/config/metadata/";
+        
+        // Connection parameters
+        String url = "jdbc:mysql://172.16.0.14:3306/CASys_DBF";
+        String username = "sa";
+        String password = "Atsp,imrtptd";
+        
+        String lsSQL = "SELECT" +
+                            "  a.sClientID" +
+                            ", a.cClientTp" +
+                            ", a.sLastName" +
+                            ", a.sFrstName" +
+                            ", a.sMiddName" +
+                            ", a.sSuffixNm" +
+                            ", a.sMaidenNm" +
+                            ", a.sCompnyNm" +
+                            ", a.cGenderCd" +
+                            ", a.cCvilStat" +
+                            ", a.sCitizenx" +
+                            ", a.dBirthDte" +
+                            ", a.sBirthPlc" +
+                            ", a.sAddlInfo" +
+                            ", a.sSpouseID" +
+                            ", a.sTaxIDNox" +
+                            ", a.sLTOIDxxx" +
+                            ", a.sPHBNIDxx" +
+                            ", a.cLRClient" +
+                            ", a.cMCClient" +
+                            ", a.cSCClient" +
+                            ", a.cSPClient" +
+                            ", a.cCPClient" +
+                            ", a.cRecdStat" +
+                            ", a.sModified" +
+                            ", a.dModified" +
+                            ", b.sTownName xBirthPlc" +
+                            ", c.sCntryNme xCitizenx" +
+                            ", d.sCompnyNm xSpouseNm" +
+                        " FROM Client_Master a" +
+                            " LEFT JOIN TownCity b ON a.sBirthPlc = b.sTownIDxx" +
+                            " LEFT JOIN Country c ON a.sCitizenx = c.sCntryCde" +
+                            " LEFT JOIN Client_Master d ON a.sSpouseID = d.sClientID" +
+                        " WHERE 0=1";
+        
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to server.");
+
+            Statement loSt = connection.createStatement();
+            ResultSet loRS = loSt.executeQuery(lsSQL);
+            String lsFileName = "Model_Client_Master.xml";
+            
+            System.setProperty("sys.default.path.metadata", DIRECTORY);
+            
+            //export column metadata to xml
+            if (MiscUtil.resultSet2XML(loRS, DIRECTORY + lsFileName)){
+                System.out.println("ResultSet exported.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
